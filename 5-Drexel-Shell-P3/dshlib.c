423e667f471c78a3598e96c293c779d32472a9da
Andy Vo <famlitbestfriends1@gmail.com>
2025-03-04 12:11:19 -0500

    Matching files

--- 5-Drexel-Shell-P3/dshlib.c
+++ 5-Drexel-Shell-P3/dshlib.c
@@ -24,20 +24,20 @@ int build_cmd_buff(char *cmd_line, cmd_buff_t *cmd_buff) {
 int build_cmd_list(char *cmd_line, command_list_t *clist) {
     if (!cmd_line || !clist) return ERR_MEMORY;
 
-    clist->num = 0;  // Reset command count
+    clist->num = 0;  
 
     char *saveptr;
     char *token = strtok_r(cmd_line, PIPE_STRING, &saveptr);
 
     while (token && clist->num < CMD_MAX) {
-        // Trim leading and trailing spaces from the token
-        while (*token == ' ') token++;  // Trim leading spaces
+        
+        while (*token == ' ') token++;  
         char *end = token + strlen(token) - 1;
-        while (end > token && *end == ' ') end--;  // Trim trailing spaces
+        while (end > token && *end == ' ') end--;  
         *(end + 1) = '\0';
 
         if (strlen(token) == 0) {
-            // Skip empty tokens (e.g., due to multiple spaces)
+            
             token = strtok_r(NULL, PIPE_STRING, &saveptr);
             continue;
         }
@@ -58,7 +58,7 @@ int execute_pipeline(command_list_t *clist) {
     int pipefds[2 * (num_commands - 1)];
     pid_t pids[num_commands];
 
-    // Create pipes
+   
     for (int i = 0; i < num_commands - 1; i++) {
         if (pipe(pipefds + 2 * i) < 0) {
             perror("pipe failed");
@@ -66,7 +66,7 @@ int execute_pipeline(command_list_t *clist) {
         }
     }
 
-    // Fork child processes
+   
     for (int i = 0; i < num_commands; i++) {
         pids[i] = fork();
         if (pids[i] < 0) {
@@ -74,35 +74,35 @@ int execute_pipeline(command_list_t *clist) {
             return ERR_EXEC_CMD;
         }
 
-        if (pids[i] == 0) { // Child process
-            // Redirect input
+        if (pids[i] == 0) {
+           
             if (i > 0) {
                 dup2(pipefds[2 * (i - 1)], STDIN_FILENO);
             }
 
-            // Redirect output
+           
             if (i < num_commands - 1) {
                 dup2(pipefds[2 * i + 1], STDOUT_FILENO);
             }
 
-            // Close all pipe file descriptors
+            
             for (int j = 0; j < 2 * (num_commands - 1); j++) {
                 close(pipefds[j]);
             }
 
-            // Execute the command
+            
             execvp(clist->commands[i].argv[0], clist->commands[i].argv);
             perror("execvp failed");
             exit(250);
         }
     }
 
-    // Close all pipe file descriptors in the parent
+    
     for (int i = 0; i < 2 * (num_commands - 1); i++) {
         close(pipefds[i]);
     }
 
-    // Wait for all child processes
+    
     for (int i = 0; i < num_commands; i++) {
         int status;
         waitpid(pids[i], &status, 0);
@@ -139,7 +139,7 @@ int exec_local_cmd_loop() {
     command_list_t clist;
 
     while (1) {
-        // Print the prompt
+        
         printf("%s", SH_PROMPT);
         fflush(stdout);
 
@@ -148,23 +148,23 @@ int exec_local_cmd_loop() {
             break;
         }
 
-        // Remove trailing newline
+        
         cmd_buff[strcspn(cmd_buff, "\n")] = '\0';
 
         if (strcmp(cmd_buff, EXIT_CMD) == 0) {
             break;
         }
 
-        // Reset the command list
+        
         memset(&clist, 0, sizeof(clist));
 
-        // Parse the command line into a list of commands
+        
         if (build_cmd_list(cmd_buff, &clist) != OK) {
             printf("%s\n", CMD_WARN_NO_CMD);
             continue;
         }
 
-        // Execute the commands
+        
         if (clist.num > 1) {
             execute_pipeline(&clist);
         } else {
